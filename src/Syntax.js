a844336af79a05d07e247a7c713469addd46c438
PINTO Angelo <apin548@darva.intra>
2018-09-10 14:54:22 +0200

    premier commit

---
+++ src/Syntax.js
@@ -0,0 +1,35 @@
+"use strict";
+exports.__esModule = true;
+var date_fns_1 = require("date-fns");
+var watch = require('tsc-watch/client');
+module.exports = function () { return ({
+    presets: [
+        require("@babel/preset-stage-2"),
+    ],
+    plugins: [
+        [require("@babel/plugin-proposal-class-properties"), { loose: true }],
+        require("@babel/plugin-proposal-object-rest-spread"),
+    ]
+}); };
+var data2 = {
+    ville: "Niort",
+    pays: "France"
+};
+function lieu(localite) {
+    return localite.ville + " en " + localite.pays;
+}
+function creeDate() {
+    var frLocale = require('date-fns/locale/fr');
+    var result = date_fns_1.format(new Date(), 'D MMMM YYYY', { locale: frLocale });
+    return result;
+}
+var data = {
+    nom: "Tom"
+};
+function bonjour(coordonnees) {
+    var partie1 = "Bonjour " + coordonnees.nom;
+    var partie2 = "nous somme le " + creeDate();
+    var partie3 = "et vous êtes actuellement à " + lieu;
+    return partie1 + " " + partie2 + " " + partie3;
+}
+console.log(bonjour(data));
---
+++ src/Syntax.ts
@@ -0,0 +1,55 @@
+import {format} from 'date-fns'
+const watch = require('tsc-watch/client');
+
+
+
+module.exports = () => ({
+  presets: [
+    require("@babel/preset-stage-2"),
+  ],
+  plugins: [
+    [require("@babel/plugin-proposal-class-properties"), { loose: true }],
+    require("@babel/plugin-proposal-object-rest-spread"),
+  ],
+});
+
+interface villePays {
+  ville: string
+  pays: string
+}
+
+const data2: villePays = {
+  ville: "Niort",
+  pays: "France"
+}
+
+function lieu (localite: villePays) {
+  return localite.ville + " en " + localite.pays
+}
+
+function creeDate (){
+  var frLocale = require('date-fns/locale/fr')
+  var result = format(
+    new Date(),
+    'D MMMM YYYY',
+    {locale: frLocale}
+  )
+
+  return result
+}
+
+interface info {
+  nom: string
+}
+
+const data: info = {
+  nom: "Tom",
+}
+
+function bonjour (coordonnees: info):string {
+  const partie1 = "Bonjour " +coordonnees.nom
+  const partie2 = "nous somme le " +creeDate()
+  const partie3 = "et vous êtes actuellement à " +lieu
+  return partie1 + " " + partie2 + " " + partie3
+}
+console.log (bonjour (data))
